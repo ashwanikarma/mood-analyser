be6e843fec3da449149e88b0efedb0a172e3ca41
ashwanikarma <ashwanikarma9@gmail.com>
2023-02-05 17:25:06 +0530

    [Ashwani] - Added analyse mood function which returns string

---
+++ MoodAnalyserProblem/MoodAnalyser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    public class MoodAnalyser
+    {
+        public string analyseMood(string message)
+        {
+            if (message.Contains("Sad"))
+                return "Sad";
+            else
+                return "Happy";
+        }
+    }
+}
--- MoodAnalyserProblem/Program.cs
+++ MoodAnalyserProblem/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Mood Analyzer Program");
+
+            MoodAnalyser mood = new MoodAnalyser();
+            string message = "I am in Sad Mood";
+            Console.WriteLine(mood.analyseMood(message));
         }
     }
 }
